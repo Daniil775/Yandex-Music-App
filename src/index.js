f1521bd45dbc4c7635c5dc72d016ca0d1f5e0fbc
Daniil <slondaniil@gmail.com>
2025-05-04 10:14:41 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,63 @@
+let musicList = [
+    {
+        artist:"Игорь Скляр",
+        song:"Комарово",
+        src:"./music/music-1.mp3"
+    },
+    {
+        artist:"Ёлка",
+        song:"Прованс",
+        src:"./music/music-2.mp3"
+    },
+    {
+        artist:"Григорий Лепс",
+        song:"Я счастливый",
+        src:"./music/music-3.mp3"
+    },
+    {
+        artist:"Високосный год",
+        song:"Тихий огонёк",
+        src:"./music/music-4.mp3"
+    },
+    {
+        artist:"Леприконсы",
+        song:"Хали-Гали, Паратрупер",
+        src:"./music/music-5.mp3"
+    }
+]
+
+let controls = {
+    play: document.querySelector('#action'),
+    next: document.querySelector('#next'),
+    prev: document.querySelector('#prev')
+}
+
+let isPlaying = false;
+let currentIndex = Math.floor(musicList.length / 2);
+let currentMusic = new Audio(musicList[currentIndex].src)
+
+function handleAction(){
+    if(isPlaying){
+        currentMusic.pause();
+        isPlaying = false;
+        controls.play.src = "./img/play.png"
+    } else{
+        currentMusic.play();
+        isPlaying = true;
+        controls.play.src = "./img/pause.png"
+    }
+}
+
+function handleNext(){
+    currentIndex<musicList.length?currentIndex++:currentIndex=0;
+    currentMusic.src = musicList[currentIndex].src;
+    currentMusic.play();
+}
+function handlePrev(){
+    currentIndex > 0 ? currentIndex-- : currentIndex= musicList.length - 1;
+    currentMusic.src = musicList[currentIndex%musicList.length].src;
+    currentMusic.play();
+}
+controls.play.addEventListener("click", handleAction)
+controls.next.addEventListener("click", handleNext)
+controls.prev.addEventListener("click", handlePrev)
